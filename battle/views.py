3b90c0a12b3195266a25a55294c3b7478160ca28
aldotele <aldo.telese@hotmail.it>
2021-06-18 10:39:04 +0200

    added success message at base deploy link

--- battle/views.py
+++ battle/views.py
@@ -6,6 +6,11 @@ from django.contrib.auth.decorators import login_required
 TEST_JSON = '{"winner":True,"army":{"S":0,"C":1,"D":2},"report":{1:{"a":3,"d":1},2:{"a":2,"d":1}}}'
 
 
+def index(request):
+    message = "Welcome! Go to --> https://browsergameteam2.herokuapp.com/accounts/google/login/"
+    return HttpResponse(message, status=200)
+
+
 def battle(request):
     #json_str = request.POST.get('data', '')
     return HttpResponse(TEST_JSON, status=200, content_type='application/json')
