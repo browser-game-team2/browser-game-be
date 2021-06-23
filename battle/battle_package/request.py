7bae0a50387fd77091ac540342cb4aec3c7329d5
David92p <dave.panetta92@gmail.com>
2021-06-23 11:59:20 +0200

    ADD class containing algorithm

--- battle/battle_package/request.py
+++ battle/battle_package/request.py
@@ -1,10 +1,12 @@
 import json
 
-class RequestFe:
+class Request:
     #We pass a string type parameter to our constructor method, which will take care of returning a useful json
     def __init__(self, strFE):
-        self.request = RequestFe.defineBattle(strFE)
+        self.request = Request.defineBattle(strFE)
 
+    
+    #The method takes a string parameter and defines a useful json to return to the battle agorithm
     @staticmethod
     def defineBattle(strFE):
         report = json.loads(strFE)
@@ -29,6 +31,6 @@ if __name__ == "__main__":
     inputFE = '{"attacker":{"type":"human","name":"player x","mail":"player@mail.com","army":{"B":5,"C":7,"D":9,"F":1},\
     "planet":"Venus"},"defender":{"type":"virtual","name":"computer 1","army":{"B":4,"C":8,"D":9,"F":1},"planet":"Mercury"}}'
     
-    request = RequestFe(inputFE)
-    print(RequestFe.test(request))
+    request = Request(inputFE)
+    print(Request.test(request))
     
