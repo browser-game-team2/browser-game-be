f7ef8f4e540c486551c98a2769fd1e960c8f719a
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-07-02 11:54:29 +0200

    [Refactoring] class "Request"

--- battle/battle_package/request.py
+++ battle/battle_package/request.py
@@ -4,19 +4,16 @@ import json
 class Request:
     # We pass a string type parameter to our constructor method,
     # which will take care of returning a useful json
-    def __init__(self, str_fe):
-        self.request = Request.define_battle(str_fe)
+    def __init__(self, request_fe):
+        self.challengers_armies = Request.define_challengers(request_fe)
 
     # The method takes a string parameter and defines a useful json to return to the battle algorithm
     @staticmethod
-    def define_battle(challengers_json_fe):
-        challengers_dict = json.loads(challengers_json_fe)
-        armies = {'attacker': challengers_dict['attacker']['army'],
-                  'defender': challengers_dict['defender']['army']}
-        return armies
-
-    def test(self):
-        return self.request
+    def define_challengers(request_fe):
+        challengers_dict = json.loads(request_fe)
+        challengers_armies = {'attacker': challengers_dict['attacker']['army'],
+                              'defender': challengers_dict['defender']['army']}
+        return challengers_armies
 
 
 # Main program
@@ -31,4 +28,4 @@ if __name__ == "__main__":
               '"army":{"S":4,"C":8,"D":9,"F":1},' \
               '"planet":"Mercury"}}'
     request = Request(inputFE)
-    print(Request.test(request))
+    print(request.challengers_armies)
