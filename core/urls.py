ec029ec4255aaaa9b79b08d442cc28dbf8c09852
aldotele <aldo.telese@hotmail.it>
2021-07-02 15:51:15 +0200

    added urls routing connection for army app

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('battle.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', include('army.urls')),
 ]
