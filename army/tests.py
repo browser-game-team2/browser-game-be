6331accee62895b087c649b8975480262fc6a7a4
Damiano Mancini <damiano.mancini1@gmail.com>
2021-07-08 16:28:01 +0200

    Added some test in test.py and ValueError check in Army Class

--- army/tests.py
+++ army/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .army_package.army import Army
+
 
 # Create your tests here.
+class ArmyTestCase(TestCase):
+    def setUp(self):
+        self.army = Army()
+        self.attacker = Army()
+        self.defender = Army()
+
+    def test_army(self):
+        self.assertIsInstance(self.army.space_ship, int)
+        self.assertIn(self.army.strategy, [1, 2, 3])
+        self.assertNotIn(self.army.strategy, [0, 4])
+
+    def test_creation_input_like_fe(self):
+        self.assertRaises(ValueError, Army.creation_input_like_fe, self.attacker, self.attacker)
