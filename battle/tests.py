0dab4cdd8da0240a34ada8d026450ceb11aa13ad
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-07-09 12:56:23 +0200

    [Added] some unit tests for "battle" app (attacker, defender, winner)

--- battle/tests.py
+++ battle/tests.py
@@ -1,3 +1,64 @@
 from django.test import TestCase
+from .battle_package.algorithm import Battle
+from .battle_package.request import Request
 
-# Create your tests here.
+
+class BattleTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.inputFE = '{"attacker": {"type": "human", ' \
+                               '"name": "player x", ' \
+                               '"mail": "player@mail.com", ' \
+                               '"army": {"S": 5, "C": 7, "D": 1, "F": 1}, ' \
+                               '"planet": "Venus"}, ' \
+                               '"defender": {"type": "virtual", ' \
+                               '"name": "computer 1", ' \
+                               '"army": {"S": 4, "C": 8, "D": 9, "F": 1}, ' \
+                               '"planet": "Mercury"}}'
+        self.request = Request(self.inputFE)
+        self.battle = Battle(self.request)
+
+    def test_battle_attacker(self):
+        self.assertIsInstance(self.battle.attacker, dict)
+        self.assertEqual(len(self.battle.attacker), 4)
+        self.assertIn('S', self.battle.attacker.keys())
+        self.assertIn('C', self.battle.attacker.keys())
+        self.assertIn('D', self.battle.attacker.keys())
+        self.assertIn('F', self.battle.attacker.keys())
+        self.assertIsInstance(self.battle.attacker['S'], int)
+        self.assertIsInstance(self.battle.attacker['C'], int)
+        self.assertIsInstance(self.battle.attacker['D'], int)
+        self.assertIsInstance(self.battle.attacker['F'], int)
+
+    def test_battle_defender(self):
+        self.assertIsInstance(self.battle.defender, dict)
+        self.assertEqual(len(self.battle.defender), 4)
+        self.assertIn('S', self.battle.defender.keys())
+        self.assertIn('C', self.battle.defender.keys())
+        self.assertIn('D', self.battle.defender.keys())
+        self.assertIn('F', self.battle.defender.keys())
+        self.assertIsInstance(self.battle.defender['S'], int)
+        self.assertIsInstance(self.battle.defender['C'], int)
+        self.assertIsInstance(self.battle.defender['D'], int)
+        self.assertIsInstance(self.battle.defender['F'], int)
+
+    def test_battle_winner(self):
+        self.assertIsInstance(self.battle.winner, bool)
+        self.assertIsInstance(self.battle.army_winner, dict)
+        self.assertEqual(len(self.battle.army_winner), 4)
+        self.assertIn('S', self.battle.army_winner.keys())
+        self.assertIn('C', self.battle.army_winner.keys())
+        self.assertIn('D', self.battle.army_winner.keys())
+        self.assertIn('F', self.battle.army_winner.keys())
+        self.assertIsInstance(self.battle.army_winner['S'], int)
+        self.assertIsInstance(self.battle.army_winner['C'], int)
+        self.assertIsInstance(self.battle.army_winner['D'], int)
+        self.assertIsInstance(self.battle.army_winner['F'], int)
+
+    def test_battle_special_round(self):
+        self.assertIn(self.battle.focus, ['S', 'C', 'D'])
+        self.assertIn(self.battle.advantage, ['a', 'd', 'n'])
+
+    def test_battle_main_battle(self):
+        pass
+
