faf60215722bd1d5430a0687e71e028b6977f69b
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-07-17 11:58:53 +0200

    [Fixed] Debug to False for production

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,7 @@ SOCIALACCOUNT_PROVIDERS = \
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=True)
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ['*']
 
